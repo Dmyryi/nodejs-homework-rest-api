c3ebf2587c4d5329018c0c0dc8da59ffa5df9297
Dmytryi <muzalevskyid333@gmail.com>
2024-02-04 20:19:06 +0200

    .

--- middlewars/isValid.js
+++ middlewars/isValid.js
@@ -1,11 +1,10 @@
 const { isValidObjectId } = require("mongoose");
-
-const { HttpError } = require("../helpers");
+const HttpError = require("../helpers/HttpError");
 
 const isValidId = (req, res, next) => {
   const { id } = req.params;
   if (!isValidObjectId(id)) {
-    next(HttpError(400, `${id} is not valid id`));
+    return next(HttpError(400, `${id} is not a valid ID`));
   }
   next();
 };
