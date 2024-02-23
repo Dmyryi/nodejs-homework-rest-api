62f3deaa5f130577723e80f8d23aa73094590c05
Dmytryi <muzalevskyid333@gmail.com>
2024-02-23 16:38:14 +0200

    .

--- collectors/verifySend.js
+++ collectors/verifySend.js
@@ -2,36 +2,40 @@ const { verifySchema } = require("../schemas/verifySchemas");
 const { User } = require("../models/user");
 const { sendEmail } = require("../helpers/sendEmail");
 
-const verifySend = async (req, res) => {
-  const { error } = verifySchema.validate(req.body);
-  if (error) {
-    error.status = 400;
-    res.status(400).json({
-      message: error.message,
-    });
-    throw error;
-  }
+const verifySend = async (req, res, next) => {
+  try {
+    const { error } = verifySchema.validate(req.body);
+    if (error) {
+      error.status = 400;
+      res.status(400).json({
+        message: error.message,
+      });
+      throw error;
+    }
 
-  const user = await User.findOne(req.body);
-  if (!user) {
-    res.status(404).json({
-      message: "Not found",
+    const user = await User.findOne(req.body);
+    if (!user) {
+      res.status(404).json({
+        message: "Not found",
+      });
+    }
+    if (user.verify) {
+      res.status(400).json({
+        message: "User already verify",
+      });
+    }
+    const mail = {
+      to: req.body,
+      subject: "Підтвердження реєстрації",
+      html: "<a href=`http://localhost:3000/users/verify/${user.verificationToken}` target=`_blank`>Нажміть для підтвердження</a>",
+    };
+    await sendEmail(mail);
+    res.json({
+      message: "Email verify resend",
     });
+  } catch (error) {
+    next(error);
   }
-  if (user.verify) {
-    res.status(400).json({
-      message: "User already verify",
-    });
-  }
-  const mail = {
-    to: req.body,
-    subject: "Підтвердження реєстрації",
-    html: "<a href=`http://localhost:3000/users/verify/${user.verificationToken}` target=`_blank`>Нажміть для підтвердження</a>",
-  };
-  await sendEmail(mail);
-  res.json({
-    message: "Email verify resend",
-  });
 };
 
 module.exports = { verifySend };
